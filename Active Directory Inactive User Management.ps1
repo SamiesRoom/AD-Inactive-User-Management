63fee665001dcf533d212b7de5b582e8f71fe536
SamiesRoom <misssamanthablundell@hotmail.com>
2024-10-24 13:14:17 +1100

    Add files via upload

---
+++ Active Directory Inactive User Management.ps1
@@ -0,0 +1,16 @@
+﻿$allUsers = Get-ADUser -Filter * -Properties Description, Enabled
+
+foreach ($user in $allUsers) {
+    if ($user.Enabled -eq $False) {
+        if ($user.Description -notlike "Inactive Account*") {
+            $newDescription = "Inactive Account $($user.Description)"
+            Set-ADUser -Identity $user -Description $newDescription
+        }
+    } else {
+        if ($user.Description -like "*Inactive Account*") {
+            $currentDescription = $user.Description
+            $newDescription = $currentDescription.Replace("Inactive Account", "  ")
+            Set-ADUser -Identity $user -Description $newDescription
+        }
+    }
+}
